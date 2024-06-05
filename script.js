04185b629bb409332890ab394391a77e3c0be4f6
Albert Celestino <162110077+1lbert@users.noreply.github.com>
2024-06-05 12:23:02 +0800

    fixed responsiveness and organized the files

---
+++ script.js
@@ -0,0 +1,10 @@
+const navMenuMobile = document.querySelector('.nav-menu-mobile')
+
+
+function showMenu() { 
+    navMenuMobile.style.display = 'flex'
+}
+function closeMenu() { 
+    navMenuMobile.style.display = 'none'
+}
+
